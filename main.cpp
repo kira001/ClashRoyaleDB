9780c5d2ec9da59f0b2649bd88b54c07a2e57720
gerardo <zargonblack@gmail.com>
2020-03-23 00:27:22 +0100

    -Gerardo //Nuove Modifiche di Test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+#include <test.h>
 #include <iostream>
 using namespace std;
 
+
+void C::fun(){
+    std::cout<<"test di prova";
+}
+
+
 int main()
 {
-     //test 2
+    //test 2
     cout<<"ciao a tutti";
     cout << "Hello World!" << endl;
     return 0;
---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef TEST_H
+#define TEST_H
+#include<iostream>
+
+class C{
+public:
+    C(){}
+    void fun();
+
+};
+
+
+#endif // TEST_H
