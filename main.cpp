3767d8ada8822e637f3dbdeb526edcbc86bdb977
gerardo <zargonblack@gmail.com>
2020-04-05 17:08:29 +0200

    Progetto

--- main.cpp
+++ main.cpp
@@ -1,17 +1,67 @@
-#include <test.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class STUDENTI{
+public:
+    virtual~STUDENTI();
+    std::string nome;
+    STUDENTI(std::string n):nome(n){}
+    virtual void F()=0;
+};
 
-void C::fun(){
-    std::cout<<"test di prova";
-}
+class UNIVERSITARIO: public STUDENTI{
+public:
+     UNIVERSITARIO(std::string n):STUDENTI(n){}
+
+    virtual void F(){
+    std::cout<<"sona uno studente universitario"; }
+};
+
+class SUPERIORI: public STUDENTI{
+public:
+  SUPERIORI(std::string n):STUDENTI(n){}
+    virtual void F(){
+    std::cout<<"sona uno studente delle superiori"; }
+};
+
+class INSEGNANTE{
+
+
+public:
+string nome;
+     INSEGNANTE(std::string n):nome(n){}
+
+        virtual void F()=0;
+    };
+
+
+
+
+class INSUNI: public INSEGNANTE{
+
+public:
+    INSUNI(std::string n):INSEGNANTE(n){}
+
+        virtual void F(){std::cout<<"sona uno studente delle superiori";}
+
+};
+
+
+
+class PERSONA: public STUDENTI,public INSEGNANTE
+{
+ public:
+ vector<STDUENTI*>
+ PERSONA()
+
+
+};
 
 
 int main()
 {
-    //test 2
-    cout<<"ciao a tutti";
-    cout << "Hello World!" << endl;
-    return 0;
+   UNIVERSITARIO A("pippo");
+
+   return 0;
 }
