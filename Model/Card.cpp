f6fca490d9297afc15970db5993fad61bda9c32b
farid <foadfffarid@gmail.com>
2020-04-21 21:05:16 +0200

    classe Card
    prima stesura della classe Card.h e Card.cpp

--- Model/Card.cpp
+++ Model/Card.cpp
@@ -1,1 +1,101 @@
+#include "Card.h"
+//Costruttori
+Card::Card(){}
+Card::Card(string n, unsigned int mana, rarity rar, unsigned int cLevel,string desc):
+           name(n),manaCost(mana),cardRarity(rar),cardLevel(cLevel),description(desc)
+           {Card::setMaxLevel(rar);}
+
+
+Card::Card(const Card& c):
+           name(c.getName()),manaCost(c.getManaCost()),cardRarity(c.getCardRarity()),cardLevel(c.getCardLevel()),description(c.getDescription()){}
+
+//SET method
+void Card::setName(string n){
+    name=n;
+}
+void Card::setManaCost(unsigned int mana){
+    manaCost=mana;
+}
+void Card::setCardRarity(Card::rarity rar){
+    cardRarity=rar;
+}
+void Card::setCardLevel(unsigned int cLevel){
+    cardLevel=cLevel;
+}
+void Card::setDescription(string desc){
+    description=desc;
+}
+void Card::setMaxLevel(Card::rarity rar){
+    switch (rar)
+       {
+         case Card::rarity::common:
+               MaxLevel=13;
+
+         case Card::rarity::rare:
+               MaxLevel=11;
+
+         case Card::rarity::epic:
+               MaxLevel=8;
+
+         case Card::rarity::legendary:
+               MaxLevel=5;
+       }
+
+}
+
+
+//GET method
+string Card::getName() const{
+    return name;
+}
+unsigned int Card::getManaCost() const {
+    return manaCost;
+}
+Card::rarity Card::getCardRarity() const {
+    return cardRarity;
+}
+unsigned int Card::getCardLevel() const {
+    return cardLevel;
+}
+string Card::getDescription() const{
+    return description;
+}
+
+unsigned int Card::getMaxLevel() const{
+    return MaxLevel;
+}
+
+
+// Conversione enum<--->string
+string Card::RarityToString() const{
+    switch (cardRarity)
+       {
+         case Card::rarity::common:
+              return "Comune";
+
+         case Card::rarity::rare:
+              return "Rara";
+
+         case Card::rarity::epic:
+              return "Epica";
+
+         case Card::rarity::legendary:
+              return "Leggendaria";
+       }
+}
+
+Card::rarity StringToRarity(string sRar){
+    if(sRar=="Comune")return  Card::rarity::common;
+
+    if(sRar=="Rara")return Card::rarity::rare;
+
+    if(sRar=="Epica")return Card::rarity::epic;
+
+return Card::rarity::legendary;
+}
+
+
+
+
+
 
--- Model/Card.h
+++ Model/Card.h
@@ -1,4 +1,45 @@
 #ifndef SPELL_H
 #define SPELL_H
 
+#include <string>
+using std::string;
+class Card
+{
+public:
+    enum rarity{common,rare,epic,legendary};
+private:
+    string name;
+    unsigned int manaCost;
+    rarity cardRarity;
+    unsigned int cardLevel;
+    string description;
+    unsigned int MaxLevel;
+public:
+    // costruttori
+    Card();
+    Card(string, unsigned int, rarity, unsigned int,string);
+    Card(const Card&);
+
+    // SET method
+    void setName(string n);
+    void setManaCost(unsigned int mana);
+    void setCardRarity(rarity rar);
+    void setCardLevel(unsigned int cLevel);
+    void setDescription(string desc);
+    void setMaxLevel(rarity rar);
+
+    //GET method
+    string getName() const;
+    unsigned int getManaCost() const;
+    rarity getCardRarity() const;
+    unsigned int getCardLevel() const;
+    string getDescription() const;
+    unsigned int getMaxLevel() const;
+
+    // Conversione enum<--->string
+    string RarityToString() const;
+    static rarity StringToRarity(std::string);
+
+
+};
 #endif // SPELL_H
