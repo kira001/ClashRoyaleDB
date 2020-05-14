1c4bc1a235196ea6371b594ccaf5505785f7050a
Ian Nicolas Di Menna <iannicolas.dimenna@gmail.com>
2020-05-14 19:55:08 +0200

    aggiungi building aggiornato

--- Model/Building.h
+++ Model/Building.h
@@ -7,12 +7,20 @@ private:
     unsigned int buildHealth;
     unsigned int lifeTime;
 public:
+
     Building();
     Building(string n, unsigned int mana, rarity rar, unsigned int cLevel,string desc, unsigned int bHealth, unsigned int lTime);
     Building(const Building& b);
+
     unsigned int getBuildHealth() const;
     void setBuildHealth(unsigned int bHealth);
     unsigned int getLifeTime() const;
     void setLifeTime(unsigned int lTime);
+
+    virtual void lvlUpgrade();
+    virtual void lvlDowngrade();
+    virtual Building* clone() const;
+
+
 };
 #endif // BUILDING_H
