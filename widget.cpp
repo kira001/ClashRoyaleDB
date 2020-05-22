acd5165c7d6ab5a6977445700f07df6d69142217
farid <foadfffarid@gmail.com>
2020-05-22 17:34:10 +0200

    struttura incompleta vista
    struttura incompleta vista

--- widget.cpp
+++ widget.cpp
@@ -1,8 +1,73 @@
 #include "widget.h"
 
+
+
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+    : QMainWindow(parent)
 {
+    mainWidget=new QWidget(this);
+    mainLayout=new QVBoxLayout(this);
+    dataLayout=new QHBoxLayout(this);
+    infoItemLayout= new QVBoxLayout(this);
+    list = new QListWidget(this);
+
+    addInfoLayout();
+    addMenu();
+    addButton();
+    mainWidget->setLayout(mainLayout);
+
+    setCentralWidget(mainWidget);
+}
+void Widget::addInfoLayout()
+{
+    QScrollArea* box= new QScrollArea();
+
+    box->setWidget(list);
+    box->setWidgetResizable(true);
+    box->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+    QScrollArea* box2= new QScrollArea();
+
+        box->setWidget(list);
+        box->setWidgetResizable(true);
+        box->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+    dataLayout->addWidget(box);
+    dataLayout->addWidget(box2);
+    mainLayout->addLayout(dataLayout);
+
+}
+
+void Widget::addMenu()
+{
+    //Creare la barra dei menu , poi il menu, poi le azioni
+    QMenuBar* menubar= new QMenuBar(this);
+    QMenu *menu =new QMenu("File",menubar);
+    QAction* save= new QAction("Save",menu);
+    QAction* load=new QAction("Load",menu);
+
+
+    //Aggiungo le azioni al menu
+    menu->addAction(save);
+    menu->addAction(load);
+
+    //Aggiungo il menu alla barra
+    menubar->addMenu(menu);
+
+    // Aggiungo la barra al Layout
+    setMenuBar(menubar);
+}
+
+void Widget::addButton()
+{
+    QHBoxLayout* buttonLayout=new QHBoxLayout(this);
+     QPushButton* insertButton = new QPushButton("Insert");
+      QPushButton* mButton = new QPushButton("aasvasv");
+      QPushButton* moButton = new QPushButton("modifica");
+     buttonLayout->addWidget(insertButton);
+     buttonLayout->addWidget(mButton);
+     buttonLayout->addWidget(moButton);
+     mainLayout->addLayout(buttonLayout);
 }
 
 Widget::~Widget()
--- widget.h
+++ widget.h
@@ -1,12 +1,26 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
-
-class Widget : public QWidget
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QScrollArea>
+#include <QListWidget>
+#include <QMenuBar>
+#include <QPushButton>
+class Widget : public QMainWindow
 {
     Q_OBJECT
+private:
+    QWidget* mainWidget;
+    QVBoxLayout* mainLayout;
+    QHBoxLayout* dataLayout;
+    QVBoxLayout* infoItemLayout;
+    QListWidget* list;
 
+    void addInfoLayout();
+    void addMenu();
+    void addButton();
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
